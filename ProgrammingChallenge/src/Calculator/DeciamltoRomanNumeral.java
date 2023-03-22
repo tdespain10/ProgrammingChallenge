4ca28ef31fd66b08cab23e63149718b3e9cd4efc
Tanner <tdespain10@gmail.com>
2023-03-22 15:37:47 -0600

    Finished!!

--- ProgrammingChallenge/src/Calculator/DeciamltoRomanNumeral.java
+++ ProgrammingChallenge/src/Calculator/DeciamltoRomanNumeral.java
@@ -16,28 +16,64 @@ public class DeciamltoRomanNumeral {
                 val = val-1000;
             }
             else if(val >= 500){
-                sb.append("D");
-                val = val-500;
+                if(val >= 900){
+                    sb.append("CM");
+                    val-=900;
+                }
+                else {
+                    sb.append("D");
+                    val = val - 500;
+                }
             }
             else if(val >= 100){
-                sb.append("C");
-                val = val-100;
+                if(val >= 400){
+                    sb.append("CD");
+                    val-=400;
+                }
+                else {
+                    sb.append("C");
+                    val = val - 100;
+                }
             }
             else if(val >= 50){
-                sb.append("L");
-                val = val-50;
+                if(val >= 90){
+                    sb.append("XC");
+                    val-=90;
+                }
+                else {
+                    sb.append("L");
+                    val = val - 50;
+                }
             }
             else if(val >= 10){
-                sb.append("X");
-                val = val-10;
+                if(val >= 40){
+                    sb.append("XL");
+                    val-=40;
+                }
+                else {
+                    sb.append("X");
+                    val = val - 10;
+                }
             }
             else if(val >= 5){
-                sb.append("V");
-                val = val-5;
+                if(val == 9){
+                    sb.append("IX");
+                    val-=9;
+                }
+                else {
+                    sb.append("V");
+                    val = val - 5;
+                }
             }
             else if(val >= 1){
-                sb.append("I");
-                val = val-1;
+                if(val == 4){
+                    sb.append("IV");
+                    val-=4;
+                }
+                else {
+                    sb.append("I");
+                    val = val - 1;
+                }
             }
         }
         setNumeral(sb.toString());
