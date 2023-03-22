eaf0d57c92030a5f6d0b5463a51a438314c39c04
Tanner <tdespain10@gmail.com>
2023-03-22 15:13:26 -0600

    numeral output is correct value but needs to be modified

--- ProgrammingChallenge/src/Calculator/DeciamltoRomanNumeral.java
+++ ProgrammingChallenge/src/Calculator/DeciamltoRomanNumeral.java
@@ -8,6 +8,39 @@ public class DeciamltoRomanNumeral {
     }
 
     public String DecimalConverter(){
+        int val = Integer.parseInt(decimal);
+        StringBuilder sb = new StringBuilder();
+        while(val>0) {
+            if (val >= 1000) {
+                sb.append("M");
+                val = val-1000;
+            }
+            else if(val >= 500){
+                sb.append("D");
+                val = val-500;
+            }
+            else if(val >= 100){
+                sb.append("C");
+                val = val-100;
+            }
+            else if(val >= 50){
+                sb.append("L");
+                val = val-50;
+            }
+            else if(val >= 10){
+                sb.append("X");
+                val = val-10;
+            }
+            else if(val >= 5){
+                sb.append("V");
+                val = val-5;
+            }
+            else if(val >= 1){
+                sb.append("I");
+                val = val-1;
+            }
+        }
+        setNumeral(sb.toString());
         return numeral;
     }
 
